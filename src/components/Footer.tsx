810e05ab9c7dcc21066e551080b54b3637262127
nischhal-nub <nischalbh51@gmail.com>
2024-05-26 13:35:44 +0545

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,14 @@ const Footer = () => {
     return (
         <div className='w-full flex bg-black mt-12 justify-center'>
             <div className='container flex flex-col'>
-                <div className='flex items-center justify-between py-5 border-b-2 border-solid border-textSecondary-200'>
+                <div className='flex flex-col md:flex-row lg:flex-row items-center justify-between py-5 border-b-2 border-solid border-textSecondary-200'>
                     <div className="about ">
                         <div className="company">
-                            <div className='flex items-center mb-6'>
+                            <div className='flex justify-center md:justify-start lg:justify-start items-center mb-6'>
                                 <img src={logo} alt="Finay logo" />
                                 <p className='font-urbanist text-2xl text-textLight font-semibold ml-4'>Finay</p>
                             </div>
-                            <p className='text-sm text-textLight w-96 font-medium font-urbanist'>The only marketplace selling unreleased music, music videos, stems, remixes, royalty rights and more as downloadable files packed into NFTs</p>
+                            <p className='text-sm text-textLight text-center md:w-96 lg:w-96 md:mb-4 lg:mb-4 md:text-left lg:text-left font-medium font-urbanist'>The only marketplace selling unreleased music, music videos, stems, remixes, royalty rights and more as downloadable files packed into NFTs</p>
                         </div>
                     </div>
                     <div className="footer-links text-textLight font-urbanist text-sm mr-0">
